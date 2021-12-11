e14f894e782634669606d72bf2254abcbde3984c
RCass172 <ruthcassidy1221@gmail.com>
2021-12-11 23:13:31 +0000

    Revert back to fix update cart bug

--- cart/urls.py
+++ cart/urls.py
@@ -5,6 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
-    path('adjust/<item_id>/', views.adjust_cart, name='adjust_cart'),
-    path('delete/<item_id>/', views.del_from_cart, name='del_from_cart'),
 ]

bac375750df1cda6b6652c40bce68b040c772272
RCass172 <ruthcassidy1221@gmail.com>
2021-12-14 23:42:40 +0000

    Set up stripe keys returns in views

--- checkout/views.py
+++ checkout/views.py
@@ -1,23 +1,43 @@
 """ Imports """
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
+import stripe
+from cart.contexts import cart_contents
 from .forms import OrderForm
 
 
 # Create your views here.
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(
             request, 'There is nothing in your shopping cart yet :(')
         return redirect(reverse('products'))
 
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(
+            request, 'Stripe public key not found in your enviroment')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51K6iStLwG2wdS70DoqxoWAZafjeCHQSDndIogoyyp3zK06hSYomHiDWMtZv1bRPwQ6oNQza3nq6Co0D5DPmTBIkM00zD6GNLuk',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)

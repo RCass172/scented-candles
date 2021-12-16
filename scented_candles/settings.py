48c9264baef0cfa673a33241973a017ea35536b2
RCass172 <ruthcassidy1221@gmail.com>
2021-12-16 22:07:25 +0000

    Add heroku deployment requirements

--- scented_candles/settings.py
+++ scented_candles/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-yd%arx3+d0(xq=6&9ib^8knp9!^kp)7l11b3r-=z^+m1rgq6be
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['scented-candles-ms4.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,12 +118,17 @@ WSGI_APPLICATION = 'scented_candles.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation

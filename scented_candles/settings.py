8082dcf7eca6b157f6c228339148584c8031f441
RCass172 <ruthcassidy1221@gmail.com>
2021-12-16 22:29:29 +0000

    Remove the secret key and set debug

--- scented_candles/settings.py
+++ scented_candles/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yd%arx3+d0(xq=6&9ib^8knp9!^kp)7l11b3r-=z^+m1rgq6be'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['scented-candles-ms4.herokuapp.com', 'localhost']
 

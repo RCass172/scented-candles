83af1d6c08b9b36c1bc3f9f322c8f5a45c81a56e
RCass172 <ruthcassidy1221@gmail.com>
2021-12-16 18:35:40 +0000

    Create the product form

---
+++ products/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Product, Category
+
+
+class productForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    image = forms.ImageField(label='Image',
+                             required=False,
+                             widget=CustomClearableFileInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'rounded-0'

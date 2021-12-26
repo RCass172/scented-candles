21b6d1935aab467c9633c8d02a74ef4566ae49cd
RCass172 <ruthcassidy1221@gmail.com>
2021-12-26 17:57:14 +0000

    Add headings to review section and hr for better aesthetics

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.del_product, name='del_product'),
-    path('add_review/<int:product_id>/', views.add_review, name='add_review'),
+    path('<int:product_id>/add_review/', views.add_review, name='add_review'),
 ]

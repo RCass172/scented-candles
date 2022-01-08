1273acff242e602c494b45ea275530c3c78e22b7
RCass172 <ruthcassidy1221@gmail.com>
2022-01-08 23:06:15 +0000

    Add additional toast messages for adjusting and removing products in cart

--- cart/views.py
+++ cart/views.py
@@ -39,13 +39,18 @@ def add_to_cart(request, item_id):
 def adjust_cart(request, item_id):
     """Adjust the quantity of product to the specified amount"""
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(
+                request, f'Updated {product.name} total is {cart[item_id]}')
     else:
         cart.pop(item_id)
+        messages.success(
+                request, f'Removed {product.name} from cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -55,14 +60,18 @@ def remove_from_cart(request, item_id):
     """Remove item from the shopping cart"""
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         cart = request.session.get('cart', {})
 
         cart.pop(item_id)
+        messages.success(
+                request, f'Removed {product.name} from cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
 
 
